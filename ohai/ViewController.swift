615c7d8fa2920dd8f922cfb0bcaec77ba76be868
Tyoma Kazakov <tyoma@kzkv.ru>
2016-05-09 22:15:28 +0300

    Initial Commit

---
+++ ohai/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ohai
+//
+//  Created by Tyoma Kazakov on 09.05.16.
+//  Copyright © 2016 Tom Kazakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
